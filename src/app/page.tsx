13fbe0f4f1648ecd910221d08d699934b26c4743
dennisong45 <dennisongwenghan@gmail.com>
2023-07-30 13:34:17 -0700

    feat: first push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,161 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
+import "./css/styles.css";
 
 export default function Home() {
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow p-4">
+        <div className="container mx-auto">
+          <div className="flex items-center justify-between py-6 lg:py-10">
+            <Link href="/" className="flex items-center">
+              <p className="hidden font-body text-2xl font-bold text-primary dark:text-white lg:block">
+                Dennis
+              </p>
+            </Link>
+            <div className="flex items-center lg:hidden">
+              <i
+                className={`bx mr-8 cursor-pointer text-3xl text-primary dark:text-white `}
+              />
+
+            </div>
+            <div className="hidden lg:block">
+              <ul className="flex items-center">
+                {["/resume", "/uses", "/contact"].map((path, index) => (
+                  <li key={index} className="group relative mr-6 mb-1">
+                    <div className="absolute left-0 bottom-0 z-20 h-0 w-full opacity-75 transition-all group-hover:h-2 group-hover:bg-yellow" />
+                    <Link
+                      href={path}
+                      className="relative z-30 block px-2 font-body text-lg font-medium text-primary transition-colors group-hover:text-green dark:text-white dark:group-hover:text-secondary"
+                    >
+                      {path.replace("/", "") || "Intro"}
+                    </Link>
+                  </li>
+                ))}
+                <li>
+                  <i
+                    className={`bx cursor-pointer text-3xl text-primary dark:text-white `}
+                  />
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
+        <div>
+          <div className="w-full">
+            <div className="justify-center container flex p-6 mx-auto border-b border-grey-lighter py-16 lg:py-20"  >
+              <div className="">
+                <Image
+                  src="/dennishanong.jpg"
+                  alt="author"
+                  width={300}
+                  height={300}
+                  className="max-w-full h-auto"
+                />
+                <h1 className="pt-3 font-body text-4xl font-semibold text-primary dark:text-white md:text-5xl lg:text-6xl">
+                  Hi, I'm Dennis.
+                </h1>
+                <p className="pt-3 font-body text-xl font-light text-primary dark:text-white">
+                  A software engineer that loves to build stuff with his
+                  keypress.
+                </p>
+              </div>
+            </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+          </div>
+        </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="flex flex-wrap container mx-auto border-b border-grey-lighter">
+          {/* First Row */}
+          <div className="flex-1 p-10">
+            <div className="w-10/12">
+              <h2 className="text-2xl font-bold text-left">
+                  American Express - Software Engineer II 
+              </h2>
+              <span>
+              2019 - Current
+              </span>
+              <p className="p-2 text-left">
+              ◦ Fraud Algorithm Context Team: Made critical decisions to
+              ensure project and application success and met business needs.
+              Designed and implemented a distributed graph solution that
+              processed millions of nodes and edges in real-time, resulting in
+              a 30 percent reduction in resource usage using Java and Vert.x
+              data structures and leverage Couchbase <br/><br/>
+              ◦ Privacy Compliance: Implement robust compliance solutions,
+              specifically targeting cookie consent and privacy regulations in
+              EU nations and California. Influenced the user experience and
+              privacy controls of over 56 million users, successfully managing
+              and processing opt-out requests for millions of records on a
+              monthly basis, thus upholding our commitment to user data privacy
+              and choice.<br/><br/>
+              ◦ Enhance: Integrate enrichment data for customers from the
+              back-end standpoint for fraudulent teams. Allow the fraud team
+              to have more context on customers spending, and increase the
+              fraudulent catch rate by 30 percent.<br/><br/>
+              Unit-Test: Writing automated tests in Java Junit and Node
+              Playwright. Reduced Regression Testing by 50 percent faster by
+              running using multiple-threads and async methods. <br/><br/>
+              ◦ Vert.x and Spring-boot: Refactor Legacy application that used
+              Spring Boot, which had become complex, hard to maintain, and
+              difficult to scale. Refactored the code-base and migrated the
+              application to Vert.x, which provided a more lightweight,
+              reactive, and event-driven architecture.<br/><br/>
+              ◦ Technologies Used: Java , Spring-boot, Vert.x, Couchbase, AWS,
+              GitHub , React, Jenkins, Openshift, Kubernetes, Dockers, Liquid
+              Jekyll, Node.js, Github-Actions
+              </p>
+            </div>
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="flex-2 w-2/12 items-start">
+            <div className="p-10 items-end">
+            <a target="_blank" href="https://www.americanexpress.com/">
+              <Image src="/amex.png" alt="resume" width={600} height={300} />
+              </a>
+            </div>
+          </div>
+        </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+        <div className="flex flex-wrap container mx-auto border-b border-grey-lighter">
+          {/* First Row */}
+          <div className="flex-1 p-10">
+            <div className="w-10/12">
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+              <h2 className="text-2xl font-bold text-left">
+                BioforceTech - Embedded Systems Engineer Intern
+
+              </h2>
+              <span>
+              Summer,Winter - 2020,2021
+              </span>
+
+              <p className="p-2 text-left">
+              ◦ Data Read/Write IoT: Develop algorithms to increase read/write speed of IoT Hardware by 30 percent by optimizing Firmware in C++ and Linux Operating System
+              <br/><br/>
+              ◦ IoT Networking : Implement a reverse tunnel feature synchronize that only open it to wide area network through the cloud once activated. Allow IoT to exist only in Local Area Network without activating it.
+              <br/><br/>
+              ◦ Cloud: Developed a Cloud platform that allows user to integrate multiple IoT into it. Hosting in AWS. Set up load-balancing, Laravel Object Oriented and RESTFUL API
+              <br/><br/>
+              Technologies Used: PHP, Laravel, Javascript, AWS, GitHub , React, C++, C , Linux , Bash
+               <br/><br/>
+              </p>
+            </div>
+          </div>
+
+          <div className="flex-2 w-2/12 items-start">
+            <div className="p-10 items-end">
+              <a target="_blank" href="https://www.bioforcetech.com/">
+              <Image src="/bft.png" alt="resume" width={600} height={300} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className="p-4 bg-blue-500 text-white"></footer>
+    </div>
+  );
 }
